ef0a5cebc2141c0b18c91e8d5d862fbae4fc3ae4
giorgiameffe <giorgiameffe03@gmail.com>
2025-03-25 17:11:27 +0100

    feat: complete bonus

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Hello Boolean');
+// Modificare app Node in modo da passare dal terminale una frase diversa da “Hello Boolean”.
+
+// console.log(process.argv);
+
+// se il valore è undefined o null stampo 'Hello Boolean'
+// altrimenti il valore viene modificato dal terminale
+
+const newText = process.argv[2] ?? 'Hello Boolean';
+console.log(newText);
